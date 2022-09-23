2c7ebb6dfe2f068e22880ad0b10b4f57b14e09d1
Josecarlos-12 <85040568+Josecarlos-12@users.noreply.github.com>
2022-09-23 03:57:54 -0500

    Pantalla de genero

---
+++ Proyecto_BCP_GameJam/Assets/Script/SelectionManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class SelectionManager : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public List<Sprite> genders = new List<Sprite>();
+    private int selectedGender = 0;
+    public GameObject playergender;
+
+
+    public void NextOption()
+    {
+        selectedGender = selectedGender + 1;
+        if (selectedGender == genders.Count)
+        {
+            selectedGender = 0;
+        }
+        sr.sprite = genders[selectedGender];
+    }
+
+    public void BackOption()
+    {
+        selectedGender = selectedGender - 1;
+        if (selectedGender < 0)
+        {
+            selectedGender = genders.Count-1;
+        }
+        sr.sprite = genders[selectedGender];
+    }
+
+    public void PlayGame()
+    {
+        PrefabUtility.SaveAsPrefabAsset(playergender,"Assets/selectedskin.prefab");
+        SceneManager.LoadScene("MainGame");
+    }
+
+}
