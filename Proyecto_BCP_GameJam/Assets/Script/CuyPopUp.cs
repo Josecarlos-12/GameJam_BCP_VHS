c1ea69349d2e4e177acb46064ac209a45171e655
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2022-09-25 06:26:33 -0500

    Pop up prestamo_ pagar_salud

--- Proyecto_BCP_GameJam/Assets/Script/CuyPopUp.cs
+++ Proyecto_BCP_GameJam/Assets/Script/CuyPopUp.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CuyPopUp : MonoBehaviour
 {
-    public GameObject prest, returCredi;
+    public GameObject prest, pagar, returCredi, salud, tajeta, auxilio, seguro;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +27,65 @@ public class CuyPopUp : MonoBehaviour
     {
         prest.SetActive(true);
         returCredi.SetActive(false);
+    }
+
+    public void Pagar()
+    {
+        pagar.SetActive(true);
+        returCredi.SetActive(false);
+    }
+
+    public void ReturnPagar()
+    {
+        pagar.SetActive(false);
+        returCredi.SetActive(true);
+    }
+
+    
+    public void ReturnSalud()
+    {
+        salud.SetActive(false);
+        returCredi.SetActive(true);
+    }
+
+    public void Salud()
+    {
+        salud.SetActive(true);
+        returCredi.SetActive(false);
+    }
+
+    public void Tarjeta()
+    {
+        tajeta.SetActive(true);
+        returCredi.SetActive(false);
+    }
+
+    public void ReturnTarjeta()
+    {
+        tajeta.SetActive(false);
+        returCredi.SetActive(true);
+    }
+
+    public void Auxilio()
+    {
+        auxilio.SetActive(true);
+        returCredi.SetActive(false);
+    }
+    public void ReturAuxilio()
+    {
+        auxilio.SetActive(false);
+        returCredi.SetActive(true);
+    }
+
+    public void Seguro()
+    {
+        seguro.SetActive(true);
+        returCredi.SetActive (false);
+    }
+
+    public void ReturnSeguro()
+    {
+        seguro.SetActive(false);
+        returCredi.SetActive(true);
     }
 }
